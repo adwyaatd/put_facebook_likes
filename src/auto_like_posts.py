fc5c22b5a44fb86dd878d74bed4d68240da5ab3c
細田来夢 <52363465+adwyaatd@users.noreply.github.com>
2021-12-19 15:35:00 +0900

    ヘッドレスを追加

--- src/auto_like_posts.py
+++ src/auto_like_posts.py
@@ -3,7 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from time import sleep
+import sys
 import traceback
+
+def headless_chrome():
+  options = webdriver.ChromeOptions()
+  # options.binary_location = "/usr/local/bin/chromedriver"
+  options.add_argument("--headless")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--lang=ja-JP")
+  options.add_argument("--single-process")
+  # options.add_argument("--disable-gpu")
+  options.add_argument("--window-size=1280x1696")
+  options.add_argument("--disable-application-cache")
+  options.add_argument("--disable-infobars")
+  # options.add_argument("--hide-scrollbars")
+  # options.add_argument("--enable-logging")
+  # options.add_argument("--log-level=0")
+  # options.add_argument("--ignore-certificate-errors")
+  options.add_argument("--homedir=/tmp")
+  options.add_argument("start-maximized")
+  options.add_argument("--disable-extensions")
+  options.add_argument("--disable-dev-shm-usage")
+
+  driver = webdriver.Chrome("/usr/local/bin/chromedriver",options=options)
+  return driver
 
 def is_findable_element(driver,attribute,attribute_value,target=None):
   print(attribute_value + "is_findable_element入った")
@@ -20,7 +44,7 @@ def is_findable_element(driver,attribute,attribute_value,target=None):
 
 def run(driver):
   try:
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(10)
 
     driver.get("https://m.facebook.com/akihiro.nishino.16?groupid=157664324853695")
 
@@ -28,6 +52,8 @@ def run(driver):
     login_button.click()
 
     email_col = driver.find_element_by_id("m_login_email")
+    print(email_col)
+    print(type(email_col))
     email_col.send_keys("09035380252")
 
     password_col = driver.find_element_by_id("m_login_password")
@@ -69,33 +95,42 @@ def run(driver):
 
     like_btns = driver.find_elements_by_link_text("いいね！")
     like_btns_cnt = len(like_btns)
-    print(like_btns_cnt)
+    print("like_btns:" + str(like_btns_cnt) + "個")
 
-    for like_btn in like_btns:
-      print("like_btn.value_of_css_property(color)")
-      print(like_btn.value_of_css_property("color"))
-      if like_btn.value_of_css_property("color") == "rgba(32, 120, 244, 1)" or like_btn.value_of_css_property("color")== "rgba(88, 144, 255, 1)":
-        print("next")
+    for like_btn_num,like_btn in enumerate(like_btns,1):
+      print("no.{}: ".format(like_btn_num), end = "")
+      try:
+        if like_btn.value_of_css_property("color") == "rgba(32, 120, 244, 1)" or like_btn.value_of_css_property("color")== "rgba(88, 144, 255, 1)":
+          print("next")
+          next
+          # sleep(1)
+        else:
+          like_btn.click()
+          print("click like_btn")
+          sleep(0.8)
+      except:
+        print("エラー")
         next
-      else:
-        like_btn.click()
-        print("click like_btn")
-        sleep(1)
-  except Exception as e:
-    print(e)
+  except Exception:
     raise
   else:
     print("All success")
 
-driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+print('1')
+
+driver = headless_chrome()
 
 try:
   run(driver)
 except Exception as e:
-  print(e)
   print("----------")
-  traceback.print_tb(e)
-  sleep(60)
+  type_, value, traceback_ = sys.exc_info()
+  print(type_)
+  print(value)
+  print(traceback_)
+  print(traceback.format_exception(type_, value, traceback_))
+  print("----------")
+  # sleep(60)
   driver.quit()
 
 driver.quit()
